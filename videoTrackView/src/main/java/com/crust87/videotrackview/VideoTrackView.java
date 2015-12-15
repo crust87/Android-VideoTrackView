6cb1d4dcbd72e27950cf3ae6ae9c0595546c93be
mabi <crust87@gmail.com>
2015-12-15 10:13:32 +0900

    add default attire

--- videoTrackView/src/main/java/com/crust87/videotrackview/VideoTrackView.java
+++ videoTrackView/src/main/java/com/crust87/videotrackview/VideoTrackView.java
@@ -100,7 +100,7 @@ public class VideoTrackView extends SurfaceView implements SurfaceHolder.Callbac
         mScreenDuration = 10000f;
         mThumbnailPerScreen = 6;
         mThumbnailDuration = mScreenDuration / mThumbnailPerScreen;
-		mTrackPadding = 0;
+		mTrackPadding = mContext.getResources().getDimensionPixelOffset(R.dimen.default_track_padding);
     }
 
     private void initAttributes(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -109,7 +109,9 @@ public class VideoTrackView extends SurfaceView implements SurfaceHolder.Callbac
         mScreenDuration = typedArray.getFloat(R.styleable.VideoTrackView_screen_duration, 10000f);
         mThumbnailPerScreen = typedArray.getInteger(R.styleable.VideoTrackView_thumbnail_per_screen, 6);
         mThumbnailDuration = mScreenDuration / mThumbnailPerScreen;
-		mTrackPadding = typedArray.getDimensionPixelOffset(R.styleable.VideoTrackView_track_padding, 0);
+		
+		int lDefaultTrackPadding = mContext.getResources().getDimensionPixelOffset(R.dimen.default_track_padding);
+		mTrackPadding = typedArray.getDimensionPixelOffset(R.styleable.VideoTrackView_track_padding, lDefaultTrackPadding);
     }
 
     protected void initTrackView() {
